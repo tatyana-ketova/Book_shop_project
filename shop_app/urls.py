52e7a0e076c611ad5835fbb617c3bd7293e58f65
tatyana-ketova <tatyana.i.ketova@gmail.com>
2023-11-27 22:35:42 +0100

    add book list and book description

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('',views.home,name="home"),
-    path('main',views.main,name="main")
+
+    path('', views.main, name="main"),
+    path('layout/<int:book_id>/', views.book_detail, name='book_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop_app/views.py
+++ shop_app/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def home(request):
-    return render(request,'layout/index.html')
+    return render(request, 'layout/index.html')
+
+
 def main(request):
-    return render(request,'layout/main.html')
-    
+    books = Book.objects.all()
+    books_per_page = 8  # You can adjust this number based on your preference
+
+    paginator = Paginator(books, books_per_page)
+    page = request.GET.get('page')
+
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+
+        books = paginator.page(1)
+    except EmptyPage:
+
+        books = paginator.page(paginator.num_pages)
+
+    return render(request, 'layout/main.html', {'books': books})
+
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'layout/book_detail.html', {'book': book})
